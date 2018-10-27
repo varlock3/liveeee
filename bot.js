3291168e5b299ec5ba5b35ada8aeee7729dad56a
varlock3 <44277067+varlock3@users.noreply.github.com>
2018-10-28 00:42:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,22 +2,19 @@ const Discord = require('discord.js')
 const client = new Discord.Client();
 client.on('ready', function(){
  console.log(`Logged in as ${client.user.tag}!`);
-    
-   // var s = ['483063515981283354','483063446376677386','483063378726879232','483063354332545045','483063463179190293'];
-   var s = ['483055660209012736','480169573530861578','483055655800930315'];
-    setInterval(function (){
+   
     client.user.setPresence({
  game: { 
     type: 1,
      url: 'https://www.twitch.tv/skwadraa',
-    name: 'فعلاً إذا اكرمت اللئيم تمرد .',
-    application_id: '477187715658547201',
+    name: '- ستفقدني كثيراً اعدك بذلك .',
+    application_id: '489557365301968906',
      assets: {
-         large_image:   `${s[Math.floor(Math.random() * s.length)]}`,
+         large_image:   `505871779709976617`,
   
     }
   }
     });
-    }, 5000);
+  
 });
 client.login(process.env.BOT_TOKEN);
