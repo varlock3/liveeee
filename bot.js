c9b40603f61937a4ff83afb3e8b026c45c432b89
varlock3 <44277067+varlock3@users.noreply.github.com>
2018-11-03 01:20:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,9 +8,9 @@ client.on('ready', function(){
     type: 1,
      url: 'https://www.twitch.tv/skwadraa',
     name: 'كثرة الحاقدين تثير اعجابي .',
-    application_id: '508000864221397020',
+    application_id: '508057078401728522',
      assets: {
-         large_image:   `508013373695459329`,
+         large_image:   `508057243124760586`,
   
     }
   }
